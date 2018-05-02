76d77148c7375cc3626f54d2d57478705d7c1170
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2018-05-02 16:19:23 +0200

    fix: javascript

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 require('http').createServer((req, res) => {
  console.log('New request')
- res.end('Hello Universe ' + require('os').process.env.SECRET_PASSWORD)
+ res.end('Hello Universe ' + require('process').env.SECRET_PASSWORD)
 }).listen(8000, () => console.log('Listening'))

f6f91b58d28150f22995c31f34c3fd5fe5dd3397
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2018-05-02 16:09:50 +0200

    fix: show password

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 require('http').createServer((req, res) => {
  console.log('New request')
- res.end('Hello Universe', + require('os').hostname())
+ res.end('Hello Universe ' + require('os').process.env.SECRET_PASSWORD)
 }).listen(8000, () => console.log('Listening'))

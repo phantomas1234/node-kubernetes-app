c714e11c3a71f2764ca90f37ed92a5aab446ed49
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2018-05-02 15:15:30 +0200

    chore: test ci/cd

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 require('http').createServer((req, res) => {
  console.log('New request')
- res.end('Hello World', + require('os').hostname())
+ res.end('Hello Universe', + require('os').hostname())
 }).listen(8000, () => console.log('Listening'))
